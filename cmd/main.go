7e510cb92882e517ade526ece4297f44ee027a9a
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2026-04-11 01:58:12 +0530

    rate limiter

--- cmd/main.go
+++ cmd/main.go
@@ -9,30 +9,33 @@
 package main
 
 import (
-	"fmt"
+	"sync"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 // Identification of users -> need some sort of key to track the bucket, usually userId or Ip address
 // 1. User level rate limit + service level
 
-type store struct {
-	data map[string]GlobalBucket
-}
+var store map[string]*GlobalBucket
+var globalRate = 3
+var storeMutex sync.RWMutex
 
 type GlobalBucket struct { // 1 per user
-	capacity       float64
-	maxCapacity    float64
-	refillType     string // min/secs
-	rate           uint
-	serviceBuckets []ServiceBucket
+
+	capacity    float64
+	maxCapacity float64
+	// refillType     string // min/secs
+	serviceBuckets map[string]*ServiceBucket
+	mu             sync.Mutex // giving each bucket its own lock helps user A not blocking user B
 }
 
 type ServiceBucket struct { // multiple per user
 	maxCapacity float64
 	serviceName string
 	capacity    float64
-	refillType  string
+	// refillType  string
 	rate        uint
 	lastRequest time.Time
 }
@@ -40,13 +43,104 @@ type ServiceBucket struct { // multiple per user
 // refill methods -> Active using goroutine, passive (lazy refill)
 
 func (g *GlobalBucket) refill() {
-	g.capacity = min(g.maxCapacity, g.capacity+float64(g.rate))
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	g.capacity = min(g.maxCapacity, g.capacity+float64(globalRate))
 }
 
 func (s *ServiceBucket) refill() {
-	s.capacity += min(s.maxCapacity, float64(s.rate)*time.Since(s.lastRequest).Seconds())
+	now := time.Now()
+	duration := now.Sub(s.lastRequest).Seconds()
+	tokensToAdd := duration * float64(s.rate)
+
+	// Set the new capacity, but don't go over max
+	s.capacity = min(s.maxCapacity, s.capacity+tokensToAdd)
+	s.lastRequest = now // IMPORTANT: Update the timestamp so you don't double-refill
+}
+
+type RequestBody struct {
+	Service string `json:"service"`
+}
+
+func handleIncomingRequest(c *gin.Context) {
+	// client indentification
+	var body RequestBody
+	if err := c.ShouldBindBodyWithJSON(&body); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	ip := c.ClientIP()
+
+	storeMutex.Lock()
+	val, ok := store[ip]
+
+	if !ok {
+		store[ip] = &GlobalBucket{
+			capacity:       10,
+			maxCapacity:    10,
+			serviceBuckets: make(map[string]*ServiceBucket),
+		}
+		val = store[ip]
+	}
+
+	if val.serviceBuckets == nil {
+		val.serviceBuckets = make(map[string]*ServiceBucket)
+	}
+
+	bucket, ok := val.serviceBuckets[body.Service]
+
+	// 2. Identify the Service bucket
+	if !ok {
+		bucket = &ServiceBucket{3, body.Service, 3, 1, time.Now()}
+		val.serviceBuckets[body.Service] = bucket
+	}
+
+	storeMutex.Unlock()
+	val.mu.Lock()
+	defer val.mu.Unlock() // keep the lock for the entire check and deduct phase
+	bucket.refill()
+
+	if bucket.capacity < 1 || val.capacity < 1 {
+		// show rate limited error
+		c.JSON(429, gin.H{
+			"Error": "You are rate limited try again later",
+		})
+		return
+	} else {
+
+		bucket.capacity = bucket.capacity - 1
+		val.capacity = val.capacity - 1
+		bucket.lastRequest = time.Now()
+
+		// call the resepective endpoint using c.Next() or manual fwd
+		c.JSON(200, gin.H{
+			"reached": body.Service,
+		})
+	}
+
+}
+
+func updateGlobalBuckets() {
+	for {
+		time.Sleep(5 * time.Second)
+		storeMutex.RLock()
+		for _, bucket := range store {
+
+			bucket.refill()
+		}
+		storeMutex.RUnlock()
+
+	}
+
 }
 
 func main() {
-	fmt.Println("Hi")
+	store = make(map[string]*GlobalBucket)
+	r := gin.Default()
+
+	go updateGlobalBuckets()
+
+	r.GET("/check", handleIncomingRequest)
+
+	r.Run()
 }
